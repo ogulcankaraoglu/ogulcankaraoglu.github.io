3769fab730ec0b03f064525bdd28b7252f1c2160
ogulcan-karaoglu <oulcankaraolu@gmail.com>
2024-01-03 12:12:53 +0300

    webgl demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -232,12 +232,9 @@
    * Portfolio details slider
    */
   new Swiper('.portfolio-details-slider', {
-    speed: 400,
+    speed: 300,
     loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
+
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
